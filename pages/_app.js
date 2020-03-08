39052645af420d201899985192fd55360fafee23
LeeMoonki <shinbaek89@gmail.com>
2020-03-08 18:14:25 +0900

    헤더 아이템들 설정

--- pages/_app.js
+++ pages/_app.js
@@ -5,15 +5,14 @@ import Theme from '../theme';
 
 const Container = styled.div``;
 const Header = styled.div`
+  display: flex;
+  position: relative;
   padding: 4px;
   height: 32px;
   background-color: ${props => props.theme.colors.primary};
 `;
-const Left = styled.div`
-  display: flex;
-  justify-content: flex-start;
-
-  & > * {
+const HeaderElementsWrapper = styled.div`
+  & > .header__button, & > .header__a {
     margin-right: 4px;
     height: 32px;
     line-height: 32px;
@@ -21,6 +20,33 @@ const Left = styled.div`
     color: #fff;
     background-color: rgba(255, 255, 255, 0.3);
     border-radius: 3px;
+    z-index: 10;
+  }
+`;
+const Left = styled(HeaderElementsWrapper)`
+  display: flex;
+  flex-grow: 1;
+  justify-content: flex-start;
+`;
+const Right = styled(HeaderElementsWrapper)`
+  display: flex;
+  flex-grow: 1;
+  justify-content: flex-end;
+`;
+const Logo = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+
+  & > a {
+    color: #fff;
+    font-size: 20px;
+    font-weight: bold;
   }
 `;
 const A = styled.a`
@@ -31,6 +57,22 @@ const Button = styled.button`
     padding: 0px 8px;
   }
 `;
+const ProfileImg = styled.button`
+  display: block;
+  width: 32px;
+  height: 32px;
+  z-index: 10;
+
+  & > div {
+    width: 32px;
+    height: 32px;
+    line-height: 32px;
+    border-radius: 100%;
+    background-image: url(/default-profile.png);
+    background-size: cover;
+  }
+`;
+
 const Footer = styled.div``;
 
 
@@ -47,13 +89,30 @@ function MyApp({ Component, pageProps }) {
       <Container id="trello-container">
         <Header id="header">
           <Left>
-            <A href="/" onClick={onClickHome}>
+            <A  className="header__a" href="/" onClick={onClickHome}>
               <span>Home</span>
             </A>
-            <Button>
+            <Button className="header__button">
               <span>Boards</span>
             </Button>
           </Left>
+          <Logo>
+            <a href="/" onClick={onClickHome}>Trello Copy</a>
+          </Logo>
+          <Right>
+            <Button className="header__button">
+              <span>Create</span>
+            </Button>
+            <Button className="header__button">
+              <span>Info</span>
+            </Button>
+            <Button className="header__button">
+              <span>Notification</span>
+            </Button>
+            <ProfileImg>
+              <div></div>
+            </ProfileImg>
+          </Right>
         </Header>
         <Component {...pageProps} />
         <Footer id="footer"></Footer>
