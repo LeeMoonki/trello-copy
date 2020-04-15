52e6b3689fb9a311437d386c6e0bd5a1b1998117
LeeMoonki <shinbaek89@gmail.com>
2020-04-15 22:09:23 +0900

    보드 리스트 레이아웃

--- packages/client/pages/cards.js
+++ packages/client/pages/cards.js
@@ -2,6 +2,42 @@ import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import Layout from 'Components/MainLayout';
 
+const headerHeight =  32;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+`;
+
+const Header = styled.div`
+  padding: 8px 8px 4px;
+  &:after {
+    display: block;
+    content: "";
+    clear: both;
+  }
+`;
+
+const HeaderLeft = styled.div`
+  float: left;
+  display: flex;
+`;
+const BoardName = styled.div`
+  padding: 0 12px;
+  line-height: ${headerHeight}px;
+  cursor: pointer;
+`;
+
+const HeaderRight = styled.div`
+  float: right;
+`;
+
+const ContentWrapper = styled.div`
+  flex-grow: 1;
+`;
+
 function Cards({ boardId, boardName }) {
   const [firstLoadingEnd, setFirstLoadingEnd] = useState(true);
   useEffect(() => {
@@ -11,7 +47,21 @@ function Cards({ boardId, boardName }) {
 
   return (
     <Layout>
+      {firstLoadingEnd && (
+        <Container>
+          <Header>
+            <HeaderLeft>
+              <BoardName>
+                <span>{ boardName }</span>
+              </BoardName>
+            </HeaderLeft>
+            <HeaderRight></HeaderRight>
+          </Header>
+          <ContentWrapper>
 
+          </ContentWrapper>
+        </Container>
+      )}
     </Layout>
   );
 }
